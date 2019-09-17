2db892202071aab8bb14cf44e4b5b4431ede0c8f
erez <ekrimsky@stanford.edu>
2019-09-17 15:16:33 -0700

    updates matlab scripts

--- get_maxon_gear.m
+++ get_maxon_gear.m
@@ -1,111 +1,48 @@
 close all; clear all; clc;
 
 
-string_start = "curl 'https://www.maxongroup.com/maxon/view/category/prodfilter/gear?pn_id=ProductSearch&pn_p=";
-string_end ="&pn_ok=&_=1562696640268' -H 'Cookie: isSdEnabled=true; JSESSIONID=3206956DFF1653EFA5890CA026973095.node1; TS01c7a627=01f8768e72935505fe0be3c74c39879f50f2ac317bb2dd46b7d41ff046a185eb3e35b1bd9efca596bb39b61c18bc6aa4e2279b76b7fac202bdbf5ff1ed54b5790b04fb007b; BT_ctst=101; atuserid=%7B%22name%22%3A%22atuserid%22%2C%22val%22%3A%2230c595ec-b2c8-41ac-b96c-637a01753d73%22%2C%22options%22%3A%7B%22end%22%3A%222020-08-09T04%3A42%3A42.988Z%22%2C%22path%22%3A%22%2F%22%7D%7D; _ga=GA1.2.1284092474.1562647363; _gid=GA1.2.503805160.1562647363; BT_sdc=eyJldF9jb2lkIjoiTkEiLCJyZnIiOiJodHRwczovL3d3dy5nb29nbGUuY29tLyIsInRpbWUiOjE1NjI2NDczNjM3NDUsInBpIjowLCJldXJsIjoiaHR0cHM6Ly93d3cubWF4b25ncm91cC5jb20vbWF4b24vdmlldy9jb250ZW50L2luZGV4IiwicmV0dXJuaW5nIjowLCJldGNjX2NtcCI6Ik5BIiwic21zIjpudWxsLCJub1dTIjoiUzU5cjZtIn0%3D; _et_coid=6a2dfd84c83ed8b4a390b2f620dcff9f; BT_pdc=eyJ2aWQiOiJOQSIsImV0Y2NfY3VzdCI6MCwiZWNfb3JkZXIiOjAsImV0Y2NfbmV3c2xldHRlciI6MCwic21zIjpudWxsLCJub19zaWduYWxpemUiOmZhbHNlfQ%3D%3D; TS01780029=01f8768e72d7c3428471e9379192f439ae10786710919147393c0ce4c666af37336415bd5708a3b1d463ddce4c8d67b6d02aa5c5fc; _dc_gtm_UA-59797293-1=1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7,es-US;q=0.6,es;q=0.5' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' -H 'Accept: */*' -H 'Referer: https://www.maxongroup.com/maxon/view/category/gear?etcc_cu=onsite&etcc_med_onsite=Product&etcc_cmp_onsite=Planetary+Gearheads+(GP)&etcc_plc=Overview-Page-Gears&etcc_var=%5bcom%5d%23en%23_d_&target=filter&filterCategory=planetary' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed > ./tmp.html";
+idx = 1;
 
 
+% should change this to just dump all the motor pages where it reads them 
+mkdir('gears')
 
-idx = 1; 
+% 121 for recommended
+% 244 for all 
+
+for i = 1:244 % for india version  
 
 
-for i = 1:119   % all planetary 
-    str = [char(string_start), num2str(i), char(string_end)]; 
-    system(str);
-
-    data = fileread('tmp.html');
+    data = fileread(sprintf('./gearbox_pages/gearboxpage_%0.3d.html', i));
     data_split = split(data, "articleDesc");
-    
+
     
     for k = 3:length(data_split)
+        fprintf('Grabbing data for gearbox %d.........................\n', idx); 
 
-
-
-        gear_data = data_split{k};
-        
-        
-        next_split = split(gear_data, '</td>');
-        
-        description = next_split{1}(3:end-1);
-        shaft_size = get_num(next_split{2});  % mm
-        
-        ratio = next_split{3};
-        start_idx = find(ratio == '>', 1, 'first');
-        ratio =  ratio(start_idx+1:end-1);
-
-        max_torque = get_num(next_split{4});
-
-        price = get_num(next_split{6});
-
-
-        % split 7 might contrain data sheet 
-        specs = next_split{7}; 
-        spec_url_split = split(specs, 'href="'); 
-        spec_url = spec_url_split{2}; 
-        url_idx = find(spec_url == '"', 1, 'first'); 
-
-        spec_url = ['https://www.maxongroup.com', spec_url(1:url_idx-1), '?_=1']; 
-        spec_data = webread(spec_url); 
-
-        spec_split = split(spec_data, '</td>'); 
-
-
-        eff_str = '<td>Max. efficiency</td>';
-        weight_str = '<td>Weight</td>'; % g 
-        inertia_str = '<td>Mass inertia</td>'; % gcm^2 
-        
-        k = strfind(spec_data, eff_str);
-        eff = str2num(spec_data(k+41:k+42)); 
-
-        k2 = strfind(spec_data, weight_str);
-        %weight = str2num(spec_data(k2+31:k2+35)); 
-        weight = get_num(spec_data(k2+31:k2+35)); 
-
-        k3 = strfind(spec_data, inertia_str);       
-        %inertia = str2num(spec_data(k3+38:k3+41)); 
-        inertia = get_num(spec_data(k3+35:k3+44));
-
-
-
-        %display(eff)
-        %display(inertia)
-        %display(weight)
+        % Find the first occurence of href 
+        gear_data = char(data_split{k});
+        ref_idx = strfind(gear_data, 'href'); 
  
-        Efficiency(idx) = eff; 
-        Weight(idx) = weight; 
-        Inertia(idx) = inertia; 
-        Description(idx) = string(description);         
-        Shaft(idx) = shaft_size; 
-        %RPM(idx) = rpm;
-        Ratio(idx) = string(ratio);
-        Torque(idx) = max_torque;
-        Price(idx) = price; 
+        url_idx_start = ref_idx+6;
+        url_idx_end = url_idx_start + find(gear_data(url_idx_start:end) == '"', 1, 'first') - 2; 
+        spec_url = ['https://www.maxongroup.com', gear_data(url_idx_start:url_idx_end), '?_=1']; 
+        system(['curl ', spec_url, sprintf(' > ./gears/gear_%0.4d.html', idx)]); 
         idx = idx + 1;
-        
     end 
     
-    
 end 
-
-
-% price, wattage, voltage, rpm, torque constant, shaft size, description
-Description = Description(:);
-Torque = Torque(:);
-Price = Price(:); 
-Ratio = Ratio(:);
-Shaft = Shaft(:); 
-Efficiency = Efficiency(:);
-Weight = Weight(:); 
-Inertia = Inertia(:); 
-
-T = table(Description, Torque, Ratio, Efficiency, Weight, Inertia, Shaft, Price); 
-writetable(T,'maxon_gears.csv','WriteRowNames',true);  
-
-
-
 
 %https://www.mathworks.com/matlabcentral/answers/44049-extract-numbers-from-mixed-string
 function num = get_num(str)
     num = str2num( regexprep( str, {'\D*([\d\.]+\d)[^\d]*',...
                                     '[^\d\.]*'}, {'$1 ', ' '} ) );
+end 
+
+
+function num = get_num2(str)
+    B = regexp(str,'\d*','Match');
+    display(B)
+    gg = cell2mat(B)
+    num = str2num(gg); 
 end 
--- get_maxon_motor.m
+++ get_maxon_motor.m
@@ -1,147 +1,34 @@
 close all; clear all; clc;
 
-%string_start = "curl 'https://www.maxongroup.us/maxon/view/category/prodfilter/motor?pn_id=ProductSearch&pn_p=";
-%string_end = "&pn_ok=&_=1562705445546' -H 'Cookie: isSdEnabled=true; JSESSIONID=A38377D89B76E504B3AB3B90EA6731B1.node2; TS01c7a627=01f8768e725d19d66f63b9d093f4ec72260564868e605da907eb10a17cd8f2ffdcd0ba5b15b8e377a79288c6d0e688d1e7f0ffac26904caa542ee2eea0169bc8e71acfcc81; BT_ctst=101; atuserid=%7B%22name%22%3A%22atuserid%22%2C%22val%22%3A%22813fce16-d4b4-4387-95e1-cad7ab82e4eb%22%2C%22options%22%3A%7B%22end%22%3A%222020-08-02T15%3A59%3A00.097Z%22%2C%22path%22%3A%22%2F%22%7D%7D; _ga=GA1.2.1077307880.1562083140; _et_coid=6a2dfd84c83ed8b4a390b2f620dcff9f; BT_sdc=eyJldF9jb2lkIjoiTkEiLCJyZnIiOiJodHRwczovL3d3dy5nb29nbGUuY29tLyIsInRpbWUiOjE1NjIwODMxNDE3NTEsInBpIjowLCJldXJsIjoiaHR0cHM6Ly93d3cubWF4b25ncm91cC51cy9tYXhvbi92aWV3L2NvbnRlbnQvaW5kZXgiLCJyZXR1cm5pbmciOjAsImV0Y2NfY21wIjoiTkEiLCJzbXMiOm51bGwsIm5vV1MiOiJtOWdheEsifQ%3D%3D; BT_pdc=eyJ2aWQiOiJOQSIsImV0Y2NfY3VzdCI6MCwiZWNfb3JkZXIiOjAsImV0Y2NfbmV3c2xldHRlciI6MCwic21zIjpudWxsLCJub19zaWduYWxpemUiOmZhbHNlfQ%3D%3D; ASP.NET_SessionId=gagujq3y35p4ez4mo0t0pbli; _gid=GA1.2.1044723703.1562693225; cookieBarAgreement=true; TS01780029=01f8768e724273fcfddfe2d6f1c5e845d6e4d0cbcc9c7ee6650d0cd7cd0600910aebf8190b1ac3123209c879f4cc99fdd0e475ee9e3ea42a775972dddf7ff4a8a40df65184; _gat_UA-59797293-7=1; _gat_UA-59797293-1=1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7,es-US;q=0.6,es;q=0.5' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' -H 'Accept: */*' -H 'Referer: https://www.maxongroup.us/maxon/view/category/motor?etcc_cu=onsite&etcc_med_onsite=Product&etcc_cmp_onsite=DCX+Program&etcc_plc=Overview-Page-DC-Motors&etcc_var=%5bus%5d%23en%23_d_&target=filter&filterCategory=DCX' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed > ./tmp_motor.html";
+
+idx = 1;
 
 
+% should change this to just dump all the motor pages where it reads them 
+mkdir('motors')
 
-string_start = "curl 'https://www.maxongroup.us/maxon/view/category/prodfilter/motor?pn_id=ProductSearch&pn_p=";
-string_end = "&pn_ok=&_=1562787797405' -H 'Cookie: isSdEnabled=false; JSESSIONID=E2219EC5F650E53E508301B2284A6430.node1; TS01c7a627=01f8768e723de6f22829497168da1bcd11bad1ca856a5e1c7947c9d4c854cf03ee01b6bc6063ce67af831203014db13b93de2ecc83e1a2fcab73454642e14f19cdaac9d9af; atuserid=%7B%22name%22%3A%22atuserid%22%2C%22val%22%3A%22813fce16-d4b4-4387-95e1-cad7ab82e4eb%22%2C%22options%22%3A%7B%22end%22%3A%222020-08-02T15%3A59%3A00.097Z%22%2C%22path%22%3A%22%2F%22%7D%7D; _ga=GA1.2.1077307880.1562083140; _et_coid=6a2dfd84c83ed8b4a390b2f620dcff9f; BT_pdc=eyJ2aWQiOiJOQSIsImV0Y2NfY3VzdCI6MCwiZWNfb3JkZXIiOjAsImV0Y2NfbmV3c2xldHRlciI6MCwic21zIjpudWxsLCJub19zaWduYWxpemUiOmZhbHNlfQ%3D%3D; _gid=GA1.2.1044723703.1562693225; TS01780029=01f8768e721a5f4d74077239ead5cce01378b32ce86a5e1c7947c9d4c854cf03ee01b6bc606f931ea3b2210183c102de6df69f39bb; BT_ctst=101; BT_sdc=eyJldF9jb2lkIjoiTkEiLCJyZnIiOiIiLCJ0aW1lIjoxNTYyNzg3NzcyMjQ2LCJwaSI6MCwiZXVybCI6Imh0dHBzOi8vd3d3Lm1heG9uZ3JvdXAudXMvbWF4b24vdmlldy9jb250ZW50L2luZGV4IiwicmV0dXJuaW5nIjowLCJldGNjX2NtcCI6Ik5BIiwic21zIjpudWxsLCJub1dTIjoibTlnYXhLIn0%3D; _gat_UA-59797293-7=1; _gat_UA-59797293-1=1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7,es-US;q=0.6,es;q=0.5' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36' -H 'Accept: */*' -H 'Referer: https://www.maxongroup.us/maxon/view/category/motor?etcc_cu=onsite&etcc_med_onsite=Product&etcc_cmp_onsite=DCX+Program&etcc_plc=Overview-Page-DC-Motors&etcc_var=%5bus%5d%23en%23_d_&target=filter&filterCategory=DCX' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed > tmp_motor.html"
-
-
-idx = 1; 
+for i = 1:65 % for india version  
 
 
-for i = 1:65 % 63 for usa, 65 for switzerland idk why  
-
-    str = [char(string_start), num2str(i), char(string_end)]; 
-    [status, res] = system(str);
-    
-    data = fileread('tmp_motor.html');
-    ! head ./tmp_motor.html 
+    data = fileread(sprintf('./motor_pages/motorpage_%0.3d.html', i));
     data_split = split(data, "articleDesc");
+
     
     for k = 3:length(data_split)
-        fprintf('Scraping data for motor %d\n', idx); 
-        motor_data = data_split{k};
-        
-        
-       
-        next_split = split(motor_data, '</td>');
-        description = next_split{1}(3:end-1);
+        fprintf('Grabbing data for motor %d.........................\n', idx); 
 
-        Description(idx) = string(description); 
-        if  isempty( strfind(description, 'compact drive'))  % skip these 
-
-
-            shaft_size = get_num(next_split{2});  % mm
-            wattage = get_num(next_split{3});
-            voltage = get_num(next_split{4});
-            idle_speed = get_num(next_split{5});
-            torque_constant = get_num(next_split{6});
-            price = get_num(next_split{7});
-
-            specs = next_split{8}; 
-            spec_url_split = split(specs, 'href="'); 
-            spec_url = spec_url_split{2}; 
-            url_idx = find(spec_url == '"', 1, 'first'); 
-
-
-            % From the specs 
-            spec_url = ['https://www.maxongroup.com', spec_url(1:url_idx-1), '?_=1']; 
-            spec_data = webread(spec_url); 
-
-            %search_strings = {'Rotor interta',
-            %                  'Stall torque',
-            %                  } 
-
-
-
-            spec_split = split(spec_data, '<td class="numberedCol"></td>'); 
-           
-            no_load_rpm = get_num(spec_split{3}); 
-            no_load_current = get_num(spec_split{4}); 
-
-            max_con_torque = get_num(spec_split{6});
-
-            if isempty(max_con_torque)
-                max_con_torque = get_num2(spec_split{6});
-                display(spec_split{6})
-                display(max_con_torque)
-            end 
-
-            stall_torque = get_num(spec_split{8});
-            max_eff = get_num(spec_split{10}(1:65));
-
-            j = 0;
-            res_string = spec_split{11};
-            if isempty( strfind(spec_split{11}, 'resistance'))
-                j = 1; 
-            end 
-            resistance = get_num(spec_split{11+j}); % ohm 
-
-
-
-
-            inductance = get_num(spec_split{12+j}); % mH
-            inertia = get_num(spec_split{17+j}(1:75)); % gcm^2
-
-
-            ms_string = spec_split{24 + j};
-            while isempty( strfind(ms_string, 'Max. speed'))
-                j = j + 1; 
-                ms_string = spec_split{24 + j};
-            end 
-            max_speed = get_num(ms_string);
-
-
-            No_load_RPM(idx) = no_load_rpm;
-            No_load_current(idx) = no_load_current; 
-            Max_cont_torque(idx) = max_con_torque; 
-            Stall_torque(idx) = stall_torque;
-            Max_efficiency(idx) = max_eff; 
-            Resistance(idx) = resistance;
-            Inductance(idx) = inductance;
-            Inertia(idx) = inertia; 
-            Max_speed(idx) = max_speed; 
-
-
-            V(idx) = voltage;
-            Watt(idx) = wattage; 
-            Shaft(idx) = shaft_size; 
-            Idle_speed(idx) = idle_speed;
-            kt_mNm(idx) = torque_constant;
-            Price(idx) = price; 
-        end 
+        % Find the first occurence of href 
+        motor_data = char(data_split{k});
+        ref_idx = strfind(motor_data, 'href'); 
+ 
+        url_idx_start = ref_idx+6;
+        url_idx_end = url_idx_start + find(motor_data(url_idx_start:end) == '"', 1, 'first') - 2; 
+        spec_url = ['https://www.maxongroup.com', motor_data(url_idx_start:url_idx_end), '?_=1']; 
+        system(['curl ', spec_url, sprintf(' > ./motors/motor_%0.4d.html', idx)]); 
         idx = idx + 1;
     end 
     
 end 
-
-
-% price, wattage, voltage, rpm, torque constant, shaft size, description
-No_load_RPM = No_load_RPM(:);
-No_load_current = No_load_current(:);
-Max_cont_torque = Max_cont_torque(:);
-Stall_torque = Stall_torque(:); 
-Max_efficiency = Max_efficiency(:); 
-Resistance = Resistance(:);
-Inductance = Inductance(:); 
-Inertia = Inertia(:);
-Max_speed = Max_speed(:); 
-
-Description = Description(:);
-V = V(:);
-Watt = Watt(:);
-kt_mNm = kt_mNm(:);
-Price = Price(:); 
-Idle_speed = Idle_speed(:);
-Shaft = Shaft(:); 
-
-T = table(Description, V, Watt, Resistance, Shaft, kt_mNm, Max_speed, ...
-         No_load_RPM, No_load_current, Stall_torque, Inertia, Max_efficiency, Inductance, Price); 
-writetable(T,'maxon_dc_motors.csv','WriteRowNames',true);  
-
 
 %https://www.mathworks.com/matlabcentral/answers/44049-extract-numbers-from-mixed-string
 function num = get_num(str)
